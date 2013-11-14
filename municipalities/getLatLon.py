21038a012a0e2efc270dbdb4b4e22d031988107e
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2013-11-13 19:18:23 -0500

    Got NJ lon/lat

--- municipalities/getLatLon.py
+++ municipalities/getLatLon.py
@@ -15,16 +15,30 @@ for line in inFile:
 	repSp = re.compile('\s')
 	city = repSp.sub("+", parts[0])
 	finalStr = city + "," + states[index]
-	#url = "http://maps.google.com/maps/api/geocode/json?address=%s&sensor=false" % finalStr
-	url = "http://maps.google.com/maps/api/geocode/json?address=Clifton,NJ&sensor=false"
-	out = urllib.urlopen(url)
-	content = out.read()
-	jObj = json.loads(content)
-	print jObj["results"]
-	if len(jObj) > 0:
+	url = "http://maps.google.com/maps/api/geocode/json?address=%s&sensor=false" % finalStr
+
+	count = 0 #if after 15 tries we do not get data, ignore this city
+	content = ""
+	jObj = None
+	hasData = False
+	while not hasData and count < 15:
+		out = urllib.urlopen(url)
+		content = out.read()
+		out.close()
+		jObj = json.loads(content)
+		if "results" in jObj and len(jObj["results"]) > 0:
+			hasData = True
+
+	if hasData:
 		#take the first
 		lat = jObj["results"][0]["geometry"]["location"]["lat"]
 		lon = jObj["results"][0]["geometry"]["location"]["lng"]
-		outFile.write("%d;%d;%s;%s" % (lat, lon, parts[0], parts[1]))
+		outFile.write("%f;%f;%s;%s" % (lat, lon, parts[0], parts[1]))
 		print "Finished %d" % i
+	else:
+		print "No data for %s" % parts[0]
+
 	i += 1
+
+inFile.close()
+outFile.close()
