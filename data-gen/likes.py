a76fdb4abeb4a6c79e948a4cb9f7680cba8de33c
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2013-11-15 23:51:40 -0500

    Didn't get as much done today as I wanted, but I'll finish likes,frequents, and leftwith tom. morning

---
+++ data-gen/likes.py
@@ -0,0 +1,17 @@
+from settings import *
+
+'''
+This generation file assumes that the database already has the drinkers as well as the beers already in place.
+'''
+cxn = conn_remote()
+
+
+#get drinkers
+
+#get beers
+
+#randomly assign drinkers to beers, favor the polish ones so they appear 40% of the time
+
+#insert them into the likes table
+
+cxn.close()
