536ba9a6df85a56bff82dadfe37d031777f27ee7
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2013-11-18 20:59:49 -0500

    Started with predator

---
+++ public_html/predator.php
@@ -0,0 +1,21 @@
+<?php
+require_once("settings.php");
+if(!isset($_GET['name'])){
+	die("No predator name specified.");
+}
+$predator_name = mysqli_escape_string($cxn, trim($_GET['name']));
+$predator_img = "";
+$predator_history = NULL; //result object
+$dangerRating = 0;
+
+//Show this predator's information, and show a strike-zone map (also possibly include his location)
+
+$q = "SELECT so.name AS name, d.gender AS gender, COUNT(*) as numOffenses, d.latitude AS lat, d.longitude AS lon, MAX(SQRT(POW(d.latitude - b.latitude,2) + POW(d.longitude - b.longitude, 2))) AS radius FROM SexOffender so, Drinker d, Frequents f, Bar b WHERE so.name = f.drinker AND f.bar = b.name AND d.name=so.name AND so.name='" . $predator_name . "' GROUP BY so.name";
+$results = mysqli_query($cxn, $q) or die("Could not get sex offender's information");
+$row = mysqli_fetch_assoc($results);
+
+$imgFile = "res/images_";
+if($row['gender'] == 'M'){
+	
+}
+print_r($row);
