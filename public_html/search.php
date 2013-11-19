49ce3c37500438dc3e8c1c0beb38e5f12f170430
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2013-11-19 01:01:30 -0500

    Some of search done

---
+++ public_html/search.php
@@ -0,0 +1,51 @@
+<form action="search.php" method="GET">
+	Location: <input type="text" name="location"><br>
+	Bar name: <input type="text" name="bname">
+	<input type="submit" value="Find">		
+</form> 
+
+<?php
+/*
+TODO: add maps
+*/
+require_once("settings.php");
+$barName = "";
+$location = "";
+if(isset($_GET['bname']) && isset($_GET['location'])){
+	$barName = trim($_GET['bname']) . " "; //extra space since data has it
+	$location = trim($_GET['location']);
+	if($location != ""){
+		if(!stristr($location, "NJ") && !stristr($location, "New Jersey")){
+			$location .= ",NJ";
+		}
+		echo "<h2>Search by location</h2>";
+		//search by location
+		$results = file_get_contents("http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=" . $location);
+		$results = json_decode($results, true);
+		$lon = $results["results"][0]["geometry"]["location"]["lng"];
+		$lat = $results["results"][0]["geometry"]["location"]["lat"];
+		$q = sprintf("SELECT name, SQRT(POW(longitude - %f, 2) + POW(latitude - %f, 2)) AS dist, city FROM Bar ORDER BY dist;", $lon, $lat);
+		$results = mysqli_query($cxn, $q) or die("Could not find closest bars");
+		//select top 10
+		$i = 10;
+		while($i > 0 && $row = mysqli_fetch_assoc($results)){
+			//get the safety rating for each bar, plot on map
+			printf("%s | %f | %s<br/>" , $row['name'], $row['dist'], $row['city']);
+			$i--;
+		}
+	}
+	else if($barName != ""){
+		//search by bar
+		echo "<h2>Search by bar name</h2>";
+		$terms = implode("%", explode(" ", $barName));
+		$q = "SELECT name, city FROM Bar WHERE name LIKE '%" . $terms . "%'";
+		$results = mysqli_query($cxn, $q) or die("Could not find bars");
+		while($row = mysqli_fetch_assoc($results)){
+			printf("%s|%s<br/>", $row['name'], $row['city']);
+		}
+	}
+}
+else{
+	die("Nothing submitted");
+}
+
