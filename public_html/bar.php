61eb5eff3225b1bd208b829b245e06b5ce088040
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2013-11-19 23:56:11 -0500

    Bar is working

--- public_html/bar.php
+++ public_html/bar.php
@@ -8,13 +8,12 @@
 	//$_GET["bar"]="Stone Bottle Cellar";
 
 	$bname = trim($_GET["bar"]);
-	//$results = getOneRating($bname);
-	//if(mysqli_num_rows($results) == 0){
-	//	niceDie("Bar not found");
-	//}
-	//$row = mysqli_fetch_assoc($results);
-	//$rating = $row['rating'];
-	$rating = 9;
+	$results = getOneRating($bname);
+	if(mysqli_num_rows($results) == 0){
+		niceDie("Bar not found");
+	}
+	$row = mysqli_fetch_assoc($results);
+	$rating = $row['rating'];
 ?>
 <h1><?php echo $bname; ?></h1>
 <h2>Safety Rating: <div class="ratingCircle r<?php echo round($rating); ?>"><?php echo $rating; ?></div></h2>
@@ -28,13 +27,14 @@
 <div class="columns cf">
 	<div class="left50">
 		<h4>Beers Offered (<?php echo $numBeers; ?>)</h4>
+		<ul>
 		<?php
 			while($row = mysqli_fetch_array($query))
 			{
-				echo $row["beer"]." <b>" . $row['price'] ."</b><br>";
+				echo "<li>". $row["beer"]. "</li>";
 			}
 		?>
-		
+		</ul><br/>
 		<?php
 			$q = "SELECT beer FROM Sells WHERE bar='".$bname."' AND beer IN (SELECT A.name FROM Beer A, Manufacturer B WHERE A.manf=B.name AND B.country IN 
 					(SELECT name FROM Country WHERE prohibition='1'))";
@@ -43,52 +43,59 @@
 			$numIllegal = mysqli_num_rows($query);
 			if($numIllegal>0)
 			{
-				echo "<h1>Caution, this Bar Sells the Following Illegal Beers</h1>";
-
+				echo "<h4>Caution, this Bar Sells the Following Illegal Beers</h4><ul>";
+				while($row = mysqli_fetch_assoc($query)){
+					echo "<li>". $row["beer"]. "</li>";
+				}
+				echo "</ul>";
 			}
-			echo $numIllegal;
-
+			else{
+				echo "<h4>This Bar Sells No Illegal Beers</h4>";
+			}
 		?>
 
 	</div>
 	<div class="right50">
-		<h4>Sex Offenders who Frequent</h4>
+		<?php
+			$q = "SELECT drinker FROM Frequents WHERE bar='".$bname."' AND drinker IN (SELECT DISTINCT name FROM SexOffender)";
+
+			$query = mysqli_query($cxn, $q) or die("Query failed: ".mysqli_error($cxn));
+
+			$numOffenders = mysqli_num_rows($query);
+		?>
+		<h4>Sex Offenders who Frequent (<?php echo $numOffenders; ?>)</h4>
+		<ul>
+		<?php
+			while($row = mysqli_fetch_array($query))
+			{
+				echo "<li>" . $row["drinker"]."</li>";
+			}
+		?>
+		</ul>
 	</div>
 </div>
-<br><br>
-Recommend to a friend? [Yes/No] probably based on the rating<br><br>
-Beers Served:
 
-<br><br>
 
-<br><br>
-Sex Offenders Associated with Bar:
-<?php
-	$q = "SELECT drinker FROM Frequents WHERE bar='".$bname."' AND drinker IN (SELECT DISTINCT name FROM SexOffender)";
-
-	$query = mysqli_query($cxn, $q) or die("Query failed: ".mysqli_error($cxn));
-
-	$numOffenders = mysqli_num_rows($query);
-	echo $numOffenders;
-
-	while($row = mysqli_fetch_array($query))
-	{
-		echo $row["drinker"]."<br>";
-	}
-?>
-<br><br>
-Have any of sex offense victims that frequent this bar become sex offenders?
 <?php
 	$q = "SELECT drinker FROM Frequents WHERE bar='".$bname."' AND drinker IN 
 	(SELECT DISTINCT victim FROM SexOffender WHERE victim IN (SELECT DISTINCT name FROM SexOffender))";
 
 	$query = mysqli_query($cxn, $q) or die("Query failed: ".mysqli_error($cxn));
 	$numVicOff = mysqli_num_rows($query);
-	echo $numVicOff;
+	if($numVicOff > 0){
+		echo "<h4>The Following Victims from this Bar Later became Sex Offenders</h4><ul class='marginFix'>";
+	}
+	else{
+		echo "<h4>Victims from this Bar did not Later become Sex Offenders</h4>";
+	}
 
 	while($row = mysqli_fetch_array($query))
 	{
-		echo $row["drinker"]."<br>";
+		echo "<li>" . $row["drinker"]."</li>";
+	}
+
+	if($numVicOff > 0){
+		echo "</ul>";
 	}
 	require_once("res/bottom.php");
 ?>
