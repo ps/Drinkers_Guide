03be1c151833e509e7522575cba11e80a5e7b79d
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2013-11-21 12:14:32 -0500

    whoops

--- public_html/res/bottom.php
+++ public_html/res/bottom.php
@@ -1,6 +1,6 @@
 		</div>
 		</div>
-		<footer><small>Made By <a href="http://www.pawel.pw/" target="_blank">Pawe&lstrok; Szcurko</a> and <a href="http://kevinalbs.com" target="_blank">Kevin Albertson</a></small></footer>
+		<footer><small>Made By <a href="http://www.pawel.pw/" target="_blank">Pawe&lstrok; Szczurko</a> and <a href="http://kevinalbs.com" target="_blank">Kevin Albertson</a></small></footer>
 	</div>
 	
 </body>
