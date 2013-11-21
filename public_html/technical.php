82937af209157cde1fe3837055ff91f6c1795757
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2013-11-20 21:46:30 -0500

    Made accordian

--- public_html/technical.php
+++ public_html/technical.php
@@ -5,68 +5,69 @@
 ?>
 <script src="res/shCore.js"></script>
 <script src="res/shBrushSql.js"></script>
-<h3>Target Audience and Business Aspects</h3>
-<p>Target Audience: Drinkers and Police</p>
-<h4>Drinkers</h4>
-<ul>
-	<li>Find safe bar around them</li>
-	<li>Be aware which predators go to the same bars that they do</li>
-</ul>
-<h4>Police</h4>
-<ul>
-	<li>Find bars most likely to have sex offenders</li>
-	<li>Use prediction to find when sex offenders are going to strike next</li>
+<h3 data-link="target" class="expand-link">Target Audience and Business Aspects <span>expand</span></h3>
+<div data-box="target" class="expand-box">
+	<p>Target Audience: Drinkers and Police</p>
+	<h4>Drinkers</h4>
+	<ul>
+		<li>Find safe bar around them</li>
+		<li>Be aware which predators go to the same bars that they do</li>
+	</ul>
+	<h4>Police</h4>
+	<ul>
+		<li>Find bars most likely to have sex offenders</li>
+		<li>Use prediction to find when sex offenders are going to strike next</li>
 
-</ul>
+	</ul>
+</div>
 
+<h3 data-link="patterns" class="expand-link">Patterns <span>expand</span></h3>
+	<div data-box="patterns" class="expand-box">
+	<p>Our patterns were placed after our data was generated. The files which enforce the patterns are listed next the the pattern names below</p>
+	<h4>People who leave with underage drinkers are sex offenders</h4>
 
+	<p>This pattern states that if an older person left with an underage drinker the older person is a sex offender. (underagePattern.py)</p>
+	<pre class="brush: sql">
+	SELECT CASE WHEN (SELECT COUNT(*) FROM (
+		SELECT d1.name
+		FROM LeftWith lw, Drinker d1, Drinker d2 
+		WHERE lw.drinker1 = d1.name AND d1.age >= 21 AND lw.drinker2 = d2.name and d2.age < 21 AND NOT EXISTS(SELECT * from SexOffender s WHERE s.name = d1.name)
+		UNION
+		SELECT d1.name
+		FROM LeftWith lw, Drinker d1, Drinker d2 
+		WHERE lw.drinker2 = d1.name AND d1.age >= 21 AND lw.drinker1 = d2.name and d2.age < 21 AND NOT EXISTS(SELECT * from SexOffender s WHERE s.name = d1.name)
+		) A) = 0 THEN 'Yes'
+	ELSE 'No'
+	END AS isTrue
+	</pre>
 
-<h3>Patterns</h3>
-<p>Our patterns were placed after our data was generated. The files which enforce the patterns are listed next the the pattern names below</p>
-<h4>People who leave with underage drinkers are sex offenders</h4>
+	<h4>Bars which serve illegal beers have sex offenders (illegalBeerPattern.py)</h4>
+	<p>
+		Here is the verification of the pattern:
+	</p>
+	<pre class="brush: sql">
+	SELECT CASE WHEN 
+	(SELECT COUNT(*) 
+	FROM Sells s 
+	WHERE s.beer IN (SELECT name FROM Beer where manf in (SELECT m.name FROM Manufacturer m,Country c Where m.country = c.name AND prohibition=1))
+	AND NOT EXISTS (SELECT * FROM SexOffender WHERE bar = s.bar)) = 0 THEN 'Yes'
+	ELSE 'No'
+	END AS isTrue
+	</pre>
 
-<p>This pattern states that if an older person left with an underage drinker the older person is a sex offender. (underagePattern.py)</p>
-<pre class="brush: sql">
-SELECT CASE WHEN (SELECT COUNT(*) FROM (
-	SELECT d1.name
-	FROM LeftWith lw, Drinker d1, Drinker d2 
-	WHERE lw.drinker1 = d1.name AND d1.age >= 21 AND lw.drinker2 = d2.name and d2.age < 21 AND NOT EXISTS(SELECT * from SexOffender s WHERE s.name = d1.name)
-	UNION
-	SELECT d1.name
-	FROM LeftWith lw, Drinker d1, Drinker d2 
-	WHERE lw.drinker2 = d1.name AND d1.age >= 21 AND lw.drinker1 = d2.name and d2.age < 21 AND NOT EXISTS(SELECT * from SexOffender s WHERE s.name = d1.name)
-	) A) = 0 THEN 'Yes'
-ELSE 'No'
-END AS isTrue
-</pre>
-
-<h4>Bars which serve illegal beers have sex offenders (illegalBeerPattern.py)</h4>
-<p>
-	Here is the verification of the pattern:
-</p>
-<pre class="brush: sql">
-SELECT CASE WHEN 
-(SELECT COUNT(*) 
-FROM Sells s 
-WHERE s.beer IN (SELECT name FROM Beer where manf in (SELECT m.name FROM Manufacturer m,Country c Where m.country = c.name AND prohibition=1))
-AND NOT EXISTS (SELECT * FROM SexOffender WHERE bar = s.bar)) = 0 THEN 'Yes'
-ELSE 'No'
-END AS isTrue
-</pre>
-
-<h4>People who frequent international bars like at least one Polish beer (internationalBeerPattern.py)</h4>
-<p>Here is a verification of the pattern</p>
-<pre class="brush: sql">
-SELECT IF
-((SELECT COUNT(f.drinker) FROM Frequents f, Bar b 
-WHERE f.bar = b.name AND b.international='1' AND
-(SELECT COUNT(*) FROM Likes l WHERE l.drinker = f.drinker AND l.beer IN(
-SELECT b.name FROM Beer b, Manufacturer m
-WHERE b.manf = m.name AND m.country = 'Poland')) = 0) = 0, 'Yes', 'No') AS verification
-</pre>
-
-<h3 data-link="dataGeneration" class="box">Data Generation</h3>
-<div data-box="dataGeneration" class="box">
+	<h4>People who frequent international bars like at least one Polish beer (internationalBeerPattern.py)</h4>
+	<p>Here is a verification of the pattern</p>
+	<pre class="brush: sql">
+	SELECT IF
+	((SELECT COUNT(f.drinker) FROM Frequents f, Bar b 
+	WHERE f.bar = b.name AND b.international='1' AND
+	(SELECT COUNT(*) FROM Likes l WHERE l.drinker = f.drinker AND l.beer IN(
+	SELECT b.name FROM Beer b, Manufacturer m
+	WHERE b.manf = m.name AND m.country = 'Poland')) = 0) = 0, 'Yes', 'No') AS verification
+	</pre>
+</div>
+<h3 data-link="dataGeneration" class="expand-link">Data Generation <span>expand</span></h3>
+<div data-box="dataGeneration" class="expand-box">
 	<p>The files responsible for each portion of generation is listed next to the names below</p>
 	<h4>Cities (cities.py, city_scrape.js)</h4>
 	<ul>
@@ -120,40 +121,56 @@ WHERE b.manf = m.name AND m.country = 'Poland')) = 0) = 0, 'Yes', 'No') AS verif
 		<li>Manufacturer and Country tables followed from the scrapted data</li>
 		<li>a few countries were manually labeled as having prohibition</li>
 	</ul>
+	<h4>Consumed Table (consumed.py)</h4>
+	<ul>
+		<li>
+			the script pulled the data from the LeftWith table
+			<ul>
+			<li>the date was based on the date from the LeftWith table and the time was anywhere between 1-120 minutes before the time at which the individuals left the bar</li>
+			<li>drinker1 from LeftWith table was selected to consume 1-4 drinks</li>
+			<li>drinker2 from LeftWith table was selected to consume 4-10 drinks</li>
+			<li>only one entry was generated for each unique drinker from the LeftWith table</li>
+			</ul>	
+		</li>
+		<li>
+			the rest of data was pulled from the Drinkers table and selected those that did not appear in LeftWith
+			<ul>
+				<li>for each drinker, an assumption was made that a drinker would go to a bar 1-4 times over a course of a month</li>
+				<li>each drinker would be generated to consume anywhere between 1-10 drinks during one visit, date of consumption was random</li>
+			</ul>
+		</li>
+	</ul>
+
+	<h4>SexOffender Table (sexOffenders.py)</h4>
+	<ul>
+		<li>Out of the offenses generated, 80% of them occured on the weekend (Thursday, Friday, Saturday</li>
+		<li>Each sex offender attacked anywhere between 1-2 times</li>
+		<li>The script selected 220 random male drinkers (210 assult females, 10 assult males)</li>
+		<li>The script selected 20 random female drinkers (5 assult females, 15 assult males)</li>
+		<li>10 male victims were made into sexual offenders and assulted females</li>
+		<li>2 female victims were made into sexual offenders and assulted males</li>
+	</ul>
 </div>
-
-
-<h4>Consumed Table (consumed.py)</h4>
-<ul>
-	<li>
-		the script pulled the data from the LeftWith table
-		<ul>
-		<li>the date was based on the date from the LeftWith table and the time was anywhere between 1-120 minutes before the time at which the individuals left the bar</li>
-		<li>drinker1 from LeftWith table was selected to consume 1-4 drinks</li>
-		<li>drinker2 from LeftWith table was selected to consume 4-10 drinks</li>
-		<li>only one entry was generated for each unique drinker from the LeftWith table</li>
-		</ul>	
-	</li>
-	<li>
-		the rest of data was pulled from the Drinkers table and selected those that did not appear in LeftWith
-		<ul>
-			<li>for each drinker, an assumption was made that a drinker would go to a bar 1-4 times over a course of a month</li>
-			<li>each drinker would be generated to consume anywhere between 1-10 drinks during one visit, date of consumption was random</li>
-		</ul>
-	</li>
-</ul>
-
-<h4>SexOffender Table (sexOffenders.py)</h4>
-<ul>
-	<li>Out of the offenses generated, 80% of them occured on the weekend (Thursday, Friday, Saturday</li>
-	<li>Each sex offender attacked anywhere between 1-2 times</li>
-	<li>The script selected 220 random male drinkers (210 assult females, 10 assult males)</li>
-	<li>The script selected 20 random female drinkers (5 assult females, 15 assult males)</li>
-	<li>10 male victims were made into sexual offenders and assulted females</li>
-	<li>2 female victims were made into sexual offenders and assulted males</li>
-</ul>
-
 <script>
 SyntaxHighlighter.all();
+function toggle(link){
+	var ln = link.attr("data-link");
+	var icon = link.find("span");
+	var val = icon.html();
+	var links = $(".expand-link");
+	if(val == "collapse"){
+		//close
+		$(".expand-box[data-box=" + ln + "]").slideUp();
+		icon.html("expand");
+	}
+	else{
+		$(".expand-box[data-box=" + ln + "]").slideDown();
+		icon.html("collapse");
+	}
+}
+$(".expand-link").on("click", function(){
+	toggle($(this));
+});
+toggle($(".expand-link[data-link=target]"));
 </script>
 <?php require_once("res/bottom.php"); ?>
