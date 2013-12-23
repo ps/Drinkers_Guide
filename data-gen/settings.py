8f194f1364bd9db0f37acbea50686e87f9899028
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2013-12-23 11:57:56 -0500

    More updates

--- data-gen/settings.py
+++
@@ -1,7 +0,0 @@
-import MySQLdb
-
-datadir = "data/"
-def conn_remote():
-	db = MySQLdb.connect(host="cs336-9.cs.rutgers.edu", user="root", passwd="root", db="SAFETY_DB")
-	print "Connection made"
-	return db
